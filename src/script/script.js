4f8f1557279ae8d2f725885b60a15c1b0092d59e
Konstantsin <shulghin1991@mail.ru>
2021-12-08 11:58:54 +0300

    layout section slaider

---
+++ src/script/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1100,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.png" alt="arrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.png" alt="arrow"></button>'
+    });
+  });
