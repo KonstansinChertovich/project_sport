b79c23c1ca196705fb2bd431041c25c4bd92d151
Konstantsin <shulghin1991@mail.ru>
2021-12-09 16:58:52 +0300

    script tabs

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1100,
-        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icon/left.png" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icon/right.png" alt="arrow"></button>'
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function myToggle (item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__cart-default').eq(i).toggleClass('catalog-item__cart-default_active');
+                $('.catalog-item__cart').eq(i).toggleClass('catalog-item__cart_active');
+            });
+        });
+    }
+
+    myToggle('.catalog-item__link');
+    myToggle('.catalog-item__back');
   });
